0f956779787726a3f494e9e7087c63eb90f15c36
ImDzoleDev <153832975+ImDzole@users.noreply.github.com>
2024-09-17 10:31:32 +0200

    Autumn/Fall Update!

--- Index.js
+++ Index.js
@@ -6,9 +6,41 @@ const navLogo = document.querySelector('#navbar_logo')
 const body = document.querySelector('body')
 
 const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-    body.classList.toggle('active')
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    body.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+/* Falling Leaves */ 
+
+const fallingLeaves = document.querySelector('.falling-leaves');
+
+const leafImages = [
+    'leaves1.png',
+    'leaves2.png',
+    'leaves3.png',
+    'leaves4.png',
+];
+
+function createLeaf() {
+    const leaf = document.createElement('div');
+    leaf.classList.add('leaf');
+    leaf.style.left = `${Math.random() * 100}vw`;
+    leaf.style.animationDuration = `${Math.random() * 3 + 2}s`;
+    leaf.style.opacity = `${Math.random() * 0.5 + 0.5}`;
+
+    
+    const randomLeaf = leafImages[Math.floor(Math.random() * leafImages.length)];
+    leaf.style.backgroundImage = `url(${randomLeaf})`;
+
+    fallingLeaves.appendChild(leaf);
+
+    setTimeout(() => {
+        leaf.remove();
+    }, 5000);
 }
 
-menu.addEventListener('click', mobileMenu)
+setInterval(createLeaf, 200);
+
